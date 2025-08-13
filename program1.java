1b258d28438bc8e43604f6d7e52006afc0a80bc6
singhsakshi-01 <maxsakshi2044@gmail.com>
2025-08-13 14:23:53 +0530

    Add files via upload

---
+++ program1.java
@@ -0,0 +1,13 @@
+public class program1 {
+    static int a=2;
+    public static void main(String[]args){
+        System.out.println(Byte.MIN_VALUE+"to"+Byte.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE+"to"+Integer.MAX_VALUE);
+        System.out.println(Float.MIN_VALUE+"to"+Float.MAX_VALUE);
+        System.out.println(Double.MIN_VALUE+"to"+Double.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE+"to"+Long.MAX_VALUE);
+        System.out.println(Character.MIN_VALUE+"to"+Long.MAX_VALUE);
+        System.out.println(Boolean.FALSE+"to"+Boolean.TRUE);
+    }
+    
+}
