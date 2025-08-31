55ba3306407303df60de5d786d918504e766f7ba
Sakshi <maxsakshi2044@gmail.com>
2025-08-31 11:51:04 +0530

    commit 1

---
+++ Assignment/sumofnaturalno.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class sumofnaturalno {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int n = scanner.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        System.out.println("The sum of the first " + n + " natural numbers is: " + sum);
+        scanner.close();
+    }
+}
